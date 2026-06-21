932c31eb8733be60c0bcffb9abbb94a1d0cc7e6f
hemaamruthak-wq <hemaamruthak@gmail.com>
2026-06-21 18:10:36 +0530

    Task_3

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-function showMessage() {
-    alert("Hello! Welcome to my internship project.");
+function checkAnswer(answer){
+
+    if(answer === 'a'){
+        document.getElementById("result").innerHTML =
+        "Correct Answer!";
+    }
+    else{
+        document.getElementById("result").innerHTML =
+        "Wrong Answer!";
+    }
+}
+
+async function getJoke(){
+
+    let response = await fetch(
+        "https://official-joke-api.appspot.com/random_joke"
+    );
+
+    let data = await response.json();
+
+    document.getElementById("joke").innerHTML =
+    data.setup + "<br><br>" + data.punchline;
 }
