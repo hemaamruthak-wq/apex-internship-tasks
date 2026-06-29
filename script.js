e5093ee7ca2bc5645f0eb1f1c389f986e40ffe45
hemaamruthak-wq <hemaamruthak@gmail.com>
2026-06-29 21:07:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-function checkAnswer(answer){
-
-    if(answer === 'a'){
-        document.getElementById("result").innerHTML =
-        "Correct Answer!";
-    }
-    else{
-        document.getElementById("result").innerHTML =
-        "Wrong Answer!";
-    }
-}
-
-async function getJoke(){
-
-    let response = await fetch(
-        "https://official-joke-api.appspot.com/random_joke"
-    );
-
-    let data = await response.json();
-
-    document.getElementById("joke").innerHTML =
-    data.setup + "<br><br>" + data.punchline;
+function toggleTheme(){
+    document.body.classList.toggle("dark");
 }
